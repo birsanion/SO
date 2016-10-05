407d1e7d6a476cc495047efe0087224ab8ea286e
Birsan Ion <birsanion@yahoo.com>
2016-10-05 17:27:26 +0300

    update

--- string.c
+++ string.c
@@ -2,26 +2,20 @@
 
 int my_strlen(char *s)
 {
-       char *p = s;
+    char *p = s;
+    while (*p)
+        ++p;
 
-          while (*p)
-                    ++p;
-
-             return (p - s);
+    return (p - s);
 }
 
 int main(void)
 {
-       int i;
-          char *s[] =
-                 {
-                           "Git tutorials",
-                                 "Tutorials Point"
-                                        };
+    int i;
+    char *s[] = {"Git tutorials", "Tutorials Point"};
 
-             for (i = 0; i < 2; ++i)
+    for (i = 0; i < 2; ++i)
+        printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
 
-                    printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
-
-                return 0;
+    return 0;
 }
